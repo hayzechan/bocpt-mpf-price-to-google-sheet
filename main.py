6cc97e455edd12b3f7dccb18c75320dc2074dc3c
Hayze Chan <hayze.chan@agileexlab.net>
2022-04-24 04:31:28 +0800

    [UPDATE] using attributes from config file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from crawler import get_url_content, get_fund_price, store_data_into_google_sheet
+from config import Config
 
 if __name__ == '__main__':
-    url = 'https://www.bocpt.com/homepage/my-choice-mpf/fund-price-enquiry/'
-    file_name = 'Currency'
-    cell_range = 'A1:B'
+    url = Config.url
+    file_name = Config.file_name
+    cell_range = Config.cell_range
+    sheet = Config.sheet
     url_content = get_url_content(url)
     fund = get_fund_price(url_content)
-    store_data_into_google_sheet(file_name, cell_range, fund)
+    store_data_into_google_sheet(file_name, sheet, cell_range, fund)
