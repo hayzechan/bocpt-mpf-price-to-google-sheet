cffd405960d85eae75398c15466298430a37532c
Hayze Chan <hayze.chan@agileexlab.net>
2022-04-25 19:02:03 +0800

    [FEAT] add scheduler

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from crawler import get_url_content, get_fund_price, store_data_into_google_sheet
 from config import Config
+from apscheduler.schedulers.background import BlockingScheduler
 
-if __name__ == '__main__':
+
+def start_program():
     url = Config.url
     file_name = Config.file_name
     cell_range = Config.cell_range
@@ -9,4 +11,10 @@ if __name__ == '__main__':
     sheet = Config.sheet
     url_content = get_url_content(url, chrome_path)
     fund = get_fund_price(url_content)
-    store_data_into_google_sheet(file_name, sheet, cell_range, fund)
+    return store_data_into_google_sheet(file_name, sheet, cell_range, fund)
+
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(start_program, 'interval', hour=1)
+    scheduler.start()
