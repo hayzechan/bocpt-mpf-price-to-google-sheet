fea38a3042a3c244d810afb22f74c673f685dd9d
Hayze Chan <hayze.chan@agileexlab.net>
2022-04-28 02:52:41 +0800

    [FEAT] enable headless mode

--- crawler.py
+++ crawler.py
@@ -7,6 +7,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def get_url_content(url, binary_location):
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
     options.binary_location = binary_location
     chrome_driver_binary = Service(ChromeDriverManager().install())
     browser = webdriver.Chrome(service=chrome_driver_binary, options=options)
